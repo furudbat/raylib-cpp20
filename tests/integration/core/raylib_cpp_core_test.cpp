48320a03367a2ecb191bab3bea7ca577dcaf4e8d
furudbat <hircreacc@gmail.com>
2024-03-01 16:53:18 +0100

    Add integration tests

---
+++ tests/integration/core/raylib_cpp_core_test.cpp
@@ -0,0 +1,42 @@
+#include <tl/expected.hpp>
+
+#include "raylib-cpp.hpp"
+#include "raylib-assert.h"
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE( "basic window", "[core]" ) {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    constexpr int ScreenWidth = 800;
+    constexpr int ScreenHeight = 450;
+    raylib::Color textColor = raylib::Color::LightGray();
+
+    raylib::Window window;
+    REQUIRE(window.Init(ScreenWidth, ScreenHeight, "raylib [core] example - basic window"));
+
+    SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    REQUIRE_FALSE(window.ShouldClose());
+    // Update
+    //----------------------------------------------------------------------------------
+    // Update your variables here
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+    {
+        window.ClearBackground(RAYWHITE);
+        textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
+    }
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+
+    window.Close();
+}
